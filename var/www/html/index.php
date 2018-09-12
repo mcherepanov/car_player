bccfd6803420c4acd909df083f70ada176803e4b
mcherepanov <mcherepanov@yandex.ru>
2018-09-12 16:09:39 +0300

    restore after crash

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h3>Панель управления плеером</h3>";
+echo "<img src='images/kenwood.png' height='50'/><div id='state'></div>";
 ?>
     <script>
         window.onload = setup();
@@ -11,10 +11,10 @@ echo "<h3>Панель управления плеером</h3>";
             xhr.send(query);
             var answer = JSON.parse(xhr.responseText);
             //console.log(answer)
-            var temp_cpu = answer[answer.length-2];
+            var temp_cpu = answer[answer.length - 2];
             setDiv('temp_cpu', temp_cpu);
 
-            var picture = answer[answer.length-1];
+            var picture = answer[answer.length - 1];
             var image = new Image();
             image.src = 'data:image/jpg;base64,' + picture;
             image.height = '150';
@@ -23,19 +23,14 @@ echo "<h3>Панель управления плеером</h3>";
             div_image.appendChild(image);
 
             var state = answer[0].split(':')[1];
-            setDiv('state', state);
-
-            var title = answer[4].split(':')[1];
-            setDiv('title', title);
-
-            var artist = answer[3].split(':')[1];
-            setDiv('artist', artist);
-
-            var current_time = answer[9].split(':')[1] + ':' + answer[9].split(':')[2];
-            setDiv('current_time', current_time);
+            setDiv('state', '<h3>Статус: ' + state + '</h3>');
 
             var total_time = answer[6].split(':')[1] + ':' + answer[6].split(':')[2];
-            setDiv('total_time', total_time);
+            var artist_title = answer[3].split(':')[1] + ': ' + answer[4].split(':')[1];
+            setDiv('artist_title', artist_title + ' (' + total_time + ' )');
+
+            //var current_time = answer[9].split(':')[1] + ':' + answer[9].split(':')[2];
+            //setDiv('current_time', current_time);
 
             var left_time = answer[7].split(':')[1] + ':' + answer[7].split(':')[2];
             setDiv('left_time', left_time);
@@ -52,22 +47,74 @@ echo "<h3>Панель управления плеером</h3>";
         function setDiv(div, value) {
             document.getElementById(div).innerHTML = value;
         }
-        function selectAlbom(){
+        function selectAlbom() {
             var albom = document.getElementById('alboms').value;
             console.log(albom);
             getdata('select_albom:' + albom);
         }
 
     </script>
+    <style>
+        progress { /* контейнер */
+            -webkit-appearance: none; /* убрать вид, который задан браузером по умолчанию */
+            border: none; /* убрать рамку в Firefox */
+            width: 100%;
+            height: 20px;
+            border-radius: 3px;
+            color: #4d7198; /* цветная линия-индикатор в IE */
+            background: #eee;
+            background-image: linear-gradient(to right, #e4e4e4 1px, transparent 1px),
+            linear-gradient(rgba(0, 0, 0, .2) 1px, rgba(255, 255, 255, .4) 1px, rgba(0, 0, 0, .2));
+            background-size: 10% 100%, 100% 100%;
+        }
+
+        .progress::-webkit-progress-bar { /* контейнер в Webkit */
+            border-radius: 3px;
+            background: #eee;
+            background-image: linear-gradient(to right, #e4e4e4 1px, transparent 1px),
+            linear-gradient(rgba(0, 0, 0, .2) 1px, rgba(255, 255, 255, .4) 1px, rgba(0, 0, 0, .2));
+            background-size: 10% 100%, 100% 100%;
+        }
+
+        .progress::-moz-progress-bar { /* разноцветная линия-индикатор в Firefox */
+            border-radius: 3px 5px 5px 3px;
+            background: #4d7198;
+            background-image: linear-gradient(135deg, transparent, transparent 33%, rgba(0, 0, 0, .1) 33%, rgba(0, 0, 0, .1) 66%, transparent 66%),
+            linear-gradient(rgba(0, 0, 0, .2) 1px, rgba(255, 255, 255, .4) 1px, rgba(0, 0, 0, .2)),
+            linear-gradient(to left, #00baff, #4d7198);
+            background-size: 35px 20px, 100% 100%, 100% 100%;
+        }
+
+        .progress::-webkit-progress-value { /* разноцветная линия-индикатор в Webkit */
+            border-radius: 3px 5px 5px 3px;
+            background: #4d7198;
+            background-image: linear-gradient(135deg, transparent, transparent 33%, rgba(0, 0, 0, .1) 33%, rgba(0, 0, 0, .1) 66%, transparent 66%),
+            linear-gradient(rgba(0, 0, 0, .2) 1px, rgba(255, 255, 255, .4) 1px, rgba(0, 0, 0, .2)),
+            linear-gradient(to left, #00baff, #4d7198);
+            background-size: 35px 20px, 100% 100%, 100% 100%;
+            -webkit-animation: animate-stripes 4s linear infinite;
+        }
+
+        @-webkit-keyframes animate-stripes {
+            0% {
+                background-position: 0 0, 0 0, 0 0;
+            }
+            100% {
+                background-position: -105px 0, 0 0, 0 0;
+            }
+        }
+    </style>
 <?php
-echo "<table border='0'>
+echo "<table border='1'>
 <tr>
-<td><div id='state'></div></td><td><div id='artist'></div></td>
-<td><div id='title'></div><td rowspan='4'><div id='img'></div></td>
+<td colspan='3'><div id='artist_title'></div></td><td rowspan='3'><div id='img'></div></td>
 </td>
 </tr>
-<tr><td><div id='current_time'></div></td><td><progress id='progress'></progress></td><td><div id='left_time'></div></td></tr>
-<tr><td><div id='total_time'></div></td></tr>
+<tr>
+
+<td colspan='2'><progress id='progress'></progress></td>
+<td><div><div id='left_time'></td></div></td></tr>
+</tr>
 <tr>
 <td colspan='3'><input id='previous' type='image'src='images/previous.png' height='48' onclick='getdata(\"previous\");' />
 <input id='stop' type='image'src='images/stop.png' height='48' onclick='getdata(\"stop\");' />
@@ -77,18 +124,21 @@ echo "<table border='0'>
 <input id='eject' type='image'src='images/eject.png' height='48' onclick='getdata(\"eject\");' /></td>
 </tr>";
 $music_dir = '/media/music';
-$alboms = scandir($music_dir);array_shift($alboms);array_shift($alboms);
-echo "<tr><td>Выбрать плейлист</td></tr>
-<tr><td colspan='2'><select id='alboms' name='alboms'>";
-foreach ($alboms as $k=>$row) {
+$alboms = scandir($music_dir);
+array_shift($alboms);
+array_shift($alboms);
+echo "<tr><td>Папка</td></tr>
+<tr><td colspan='3'><select id='alboms' name='alboms'>";
+foreach ($alboms as $k => $row) {
     echo "<option>" . $row . "</option>";
 }
-echo "</select></td>
-<td><input id='select_albom' type='button' value='Выбрать' onclick='selectAlbom();'></td>
+echo "</select>
+&nbsp;&nbsp;&nbsp;<input id='select_albom' type='button' value='Выбрать' onclick='selectAlbom();'></td>
 </tr>
 <tr><td>&nbsp;</td></tr>
 <tr><td>Температура процессора</td><td><div id='temp_cpu'></div></td></tr>
 <tr>
+<tr><td>&nbsp;</td></tr><tr><td>&nbsp;</td></tr>
 <td><input id='reboot' type='button' value='Перезагрузить' onclick='getdata(\"reboot\");'></td>
 <td><input id='poweroff' type='button' value='Выключить' onclick='getdata(\"poweroff\");'></td>
 </tr>
